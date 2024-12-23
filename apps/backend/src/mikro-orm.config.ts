708c996c4c100a42fb6facd258dca0412f7a861f
Imtiaz uddin <iuashrafi@gmail.com>
2024-12-24 00:53:28 +0530

    postgres setup

--- apps/backend/src/mikro-orm.config.ts
+++ apps/backend/src/mikro-orm.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, Platform, TextType, Type } from '@mikro-orm/core';
-import { MySqlDriver } from '@mikro-orm/mysql';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { NotFoundException } from '@nestjs/common';
 import 'dotenv/config';
@@ -7,7 +7,7 @@ import 'dotenv/config';
 export default defineConfig({
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
-  driver: MySqlDriver,
+  driver: PostgreSqlDriver,
   dbName: process.env.DB_NAME as string,
   host: process.env.DB_HOST as string,
   password: process.env.DB_PASSWORD as string,
